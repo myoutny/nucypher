d6831eefcb56c9949aae1860f2b142877f55804a
myoutny <myoutny@gmail.com>
2021-06-11 18:20:31 -0700

    optimization?

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,6 +5,8 @@ import {
 } from "../generated/NU/NU"
 import { Approval, Transfer, User } from "../generated/schema"
 
+import { store, BigInt } from '@graphprotocol/graph-ts'
+
 export function handleApproval(event: ApprovalEvent): void {
   let entity = new Approval(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
@@ -24,20 +26,30 @@ export function handleTransfer(event: TransferEvent): void {
   entity.value = event.params.value
   entity.save()
 
-  let fromUser = User.load(event.params.from.toHexString());
-  if (!fromUser) {
-    fromUser = new User(event.params.from.toHexString());
-  }
-
-  let toUser = User.load(event.params.to.toHexString());
-  if (!toUser) {
-    toUser = new User(event.params.to.toHexString());
-  }
-
   let nuContract = TokenContract.bind(event.address)
-  fromUser.balance = nuContract.balanceOf(event.params.from)
-  toUser.balance = nuContract.balanceOf(event.params.to)
+  let from_balance = nuContract.balanceOf(event.params.from)
+  let to_balance = nuContract.balanceOf(event.params.to)
 
-  fromUser.save();
-  toUser.save();
+  // if balance is nonzero update user entity
+  let bigzero = new BigInt(0)
+  if ( BigInt.compare(from_balance, bigzero) == 1 ){
+    let fromUser = User.load(event.params.from.toHexString())
+    if (!fromUser) {
+      fromUser = new User(event.params.from.toHexString())
+    }
+
+    fromUser.balance = from_balance
+    fromUser.save();
+  }
+
+  if ( BigInt.compare(to_balance, bigzero) == 1 ){
+    let toUser = User.load(event.params.to.toHexString())
+    if (!toUser) {
+      toUser = new User(event.params.to.toHexString())
+    }
+
+    toUser.balance = to_balance
+    toUser.save();
+  }
+
 }
